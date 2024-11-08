62fc8ae8a2761d0a8b0a87db413d9bcd163f5520
XuTiancicn <153572778+XuTiancicn@users.noreply.github.com>
2024-11-08 22:56:51 +0800

    bata0.22
    bata0.22

--- main.kt
+++ main.kt
@@ -13,8 +13,11 @@ import java.io.File
 import java.nio.file.Files
 import java.nio.file.StandardCopyOption
 import javax.swing.JOptionPane
+import java.awt.*
+import java.awt.event.WindowAdapter
+import java.awt.event.WindowEvent
 
-fun main() {
+fun main(args: Array<String>) {
     //获取当前系统
     val os = ManagementFactory.getOperatingSystemMXBean()
     println("系统名称: ${os.name}")
@@ -35,45 +38,70 @@ fun main() {
                 System.exit(0) //退出程序
             }
     }
-    val frame = JFrame("A-program-written-for-Dalian-No.113-Middle-School")
-    frame.setSize(550, 550)
-    frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
-    //设置按钮的大小
-    val button = JButton("开始安装")
-    button.preferredSize = Dimension(150, 50)
-    //设置按钮的字体和大小
-    button.font = Font("微软雅黑", Font.BOLD, 16)
-    val panel = frame.contentPane
-    panel.layout = GridBagLayout()
-    val c = GridBagConstraints()
-    c.gridx = 0
-    c.gridy = 0
-    panel.add(button, c)
-    frame.contentPane.add(button)
-    frame.pack()
-    frame.setLocationRelativeTo(null)
-    frame.setSize(550, 550)
-    frame.isVisible = true
-    //按钮点击操作
-     button.addActionListener(object : ActionListener {
-        override fun actionPerformed(e: ActionEvent?) {
-            val sourceDir = File(".") // 当前目录
-            val targetDir = File("C:\\Program Files\\A-program-written-for-Dalian-No.113-Middle-School")
-            
-            // 如果目标文件夹不存在，则创建
-            if (!targetDir.exists()) {
-                targetDir.mkdirs()
-                }
-                
-                sourceDir.listFiles()?.filter { it.extension == "jar" }?.forEach {
-                val targetFile = File(targetDir, it.name)
-                try {
-                    Files.copy(it.toPath(), targetFile.toPath())
-                    JOptionPane.showMessageDialog(null,"安装成功!")
-                    } catch (e: Exception) {
-                    JOptionPane.showMessageDialog(null,"安装失败：${e.message}")
+     // 检查是否有 -go 参数
+    if (args.contains("-go")) {
+        // 有 -go 参数
+        println("请添加 -go 参数")
+        val frame = JFrame("请添加 -go 参数")
+        frame.setSize(550, 550)
+        frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+        //设置按钮的大小
+        val button = JButton("请添加 -go 参数")
+        button.preferredSize = Dimension(150, 50)
+        //设置按钮的字体和大小
+        button.font = Font("微软雅黑", Font.BOLD, 16)
+        val panel = frame.contentPane
+        panel.layout = GridBagLayout()
+        val c = GridBagConstraints()
+        c.gridx = 0
+        c.gridy = 0
+        panel.add(button, c)
+        frame.contentPane.add(button)
+        frame.pack()
+        frame.setLocationRelativeTo(null)
+        frame.setSize(550, 550)
+        frame.isVisible = true
+        } else {
+            val frame = JFrame("A-program-written-for-Dalian-No.113-Middle-School")
+            frame.setSize(550, 550)
+            frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+            //设置按钮的大小
+            val button = JButton("开始安装")
+            button.preferredSize = Dimension(150, 50)
+            //设置按钮的字体和大小
+            button.font = Font("微软雅黑", Font.BOLD, 16)
+            val panel = frame.contentPane
+            panel.layout = GridBagLayout()
+            val c = GridBagConstraints()
+            c.gridx = 0
+            c.gridy = 0
+            panel.add(button, c)
+            frame.contentPane.add(button)
+            frame.pack()
+            frame.setLocationRelativeTo(null)
+            frame.setSize(550, 550)
+            frame.isVisible = true
+            //按钮点击操作
+            button.addActionListener(object : ActionListener {
+                override fun actionPerformed(e: ActionEvent?) {
+                    val sourceDir = File(".") // 当前目录
+                    val targetDir = File("C:\\Program Files\\A-program-written-for-Dalian-No.113-Middle-School")
+                    
+                    // 如果目标文件夹不存在，则创建
+                    if (!targetDir.exists()) {
+                        targetDir.mkdirs()
+                    }
+                    
+                    sourceDir.listFiles()?.filter { it.extension == "jar" }?.forEach {
+                        val targetFile = File(targetDir, it.name)
+                        try {
+                        Files.copy(it.toPath(), targetFile.toPath())
+                        JOptionPane.showMessageDialog(null,"安装成功!")
+                        } catch (e: Exception) {
+                        JOptionPane.showMessageDialog(null,"安装失败：${e.message}")
+                    }
                 }
             }
-        }
-    })
+        })
+    }
 }
